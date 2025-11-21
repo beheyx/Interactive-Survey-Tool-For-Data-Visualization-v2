32e21ff5b5a73afec31a048aeece64f2ac6f4765
Wesley Trieu <wesley.trieu@gmail.com>
2025-11-21 01:56:20 -0800

    Fixed Table Displays for Published Survey

--- main/api/routes/publishedSurveys.js
+++ main/api/routes/publishedSurveys.js
@@ -19,7 +19,41 @@ router.get('/:id', requireAuthentication, handleErrors( async (req, res, next) =
 	if (req.userid == publishedSurvey.userId) {
 		res.status(200).json(publishedSurvey)
 	} else {
-		res.status(401).send({ 
+		res.status(401).send({
+			error: "You do not have access to this resource"
+		})
+	}
+}))
+
+// Update published survey (edit dates to reopen/extend)
+router.patch('/:id', requireAuthentication, handleErrors( async (req, res, next) => {
+	const publishedSurvey = await getResourceById(PublishedSurvey, req.params.id)
+
+	if (req.userid == publishedSurvey.userId) {
+		await PublishedSurvey.update(req.body, {
+			where: { id: req.params.id },
+			fields: PublishedSurveyClientFields.filter(
+				field => field !== 'userId' && field !== 'surveyDesign' && field !== 'questions' && field !== 'results'
+			)
+		})
+
+		res.status(200).send()
+	} else {
+		res.status(401).send({
+			error: "You do not have access to this resource"
+		})
+	}
+}))
+
+// Delete published survey
+router.delete('/:id', requireAuthentication, handleErrors( async (req, res, next) => {
+	const publishedSurvey = await getResourceById(PublishedSurvey, req.params.id)
+
+	if (req.userid == publishedSurvey.userId) {
+		await publishedSurvey.destroy();
+		res.status(200).send()
+	} else {
+		res.status(401).send({
 			error: "You do not have access to this resource"
 		})
 	}
