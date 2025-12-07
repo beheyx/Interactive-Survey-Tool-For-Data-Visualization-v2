5a61f1f277a64521a93f921f6b77b99f4ffb1ca3
ehtoon <htoone@oregonstate.edu>
2025-12-07 03:11:23 -0800

    Implemented question reordering

--- main/api/routes/questions.js
+++ main/api/routes/questions.js
@@ -3,6 +3,8 @@ const { Question, QuestionClientFields } = require('../model/Question')
 const { Visualization } = require('../model/Visualization')
 const { requireAuthentication } = require('../lib/auth')
 const { handleErrors, getResourceById } = require('../lib/error')
+const { SurveyDesign } = require('../model/SurveyDesign')
+const sequelize = require('../lib/sequelize')
 
 // setup express router
 const express = require('express');
@@ -10,101 +12,188 @@ const router = express.Router();
 
 // setup cookie parser
 const cookieParser = require('cookie-parser');
-const { SurveyDesign } = require('../model/SurveyDesign')
 router.use(cookieParser());
 
 // setup axios API interface for visualization engine
 const axios = require('axios');
 const visualApi = axios.create({
-    baseURL: process.env.VISUAL_API_URL
+  baseURL: process.env.VISUAL_API_URL
 })
 
+/*
+ * Get specific question info
+ */
+router.get('/:id', requireAuthentication, handleErrors(async (req, res, next) => {
+  const question = await getResourceById(Question, req.params.id)
+  const surveyDesign = await getResourceById(SurveyDesign, question.surveyDesignId)
 
-// Get specific question info
-router.get('/:id', requireAuthentication, handleErrors( async (req, res, next) => {
-	const question = await getResourceById(Question, req.params.id)
-	const surveyDesign = await getResourceById(SurveyDesign, question.surveyDesignId)
-
-	if (req.userid == surveyDesign.userId) {
-		res.status(200).json(question);
-	} else {
-		res.status(401).send({ 
-			error: "You do not have access to this resource"
-		})
-	}
+  if (req.userid == surveyDesign.userId) {
+    res.status(200).json(question);
+  } else {
+    res.status(401).send({
+      error: "You do not have access to this resource"
+    })
+  }
 }))
 
-// Delete specific question
-router.delete('/:id', requireAuthentication, handleErrors( async (req, res, next) => {
-	const question = await getResourceById(Question, req.params.id)
-	const surveyDesign = await getResourceById(SurveyDesign, question.surveyDesignId)
+/*
+ * Delete specific question
+ */
+router.delete('/:id', requireAuthentication, handleErrors(async (req, res, next) => {
+  const question = await getResourceById(Question, req.params.id)
+  const surveyDesign = await getResourceById(SurveyDesign, question.surveyDesignId)
 
-	if (req.userid == surveyDesign.userId) {
-		if (question.visualizationContentId) {
-			await visualApi.delete('/' + question.visualizationContentId)
-		}
+  if (req.userid == surveyDesign.userId) {
+    if (question.visualizationContentId) {
+      await visualApi.delete('/' + question.visualizationContentId)
+    }
 
-		await question.destroy();
+    await question.destroy();
 
-		// Update parent survey design's updatedAt timestamp
-		await surveyDesign.changed('updatedAt', true);
-		await surveyDesign.save();
+    // Update parent survey design's updatedAt timestamp
+    await surveyDesign.changed('updatedAt', true);
+    await surveyDesign.save();
 
-		res.status(200).send()
-	} else {
-		res.status(401).send({
-			error: "You do not have access to this resource"
-		})
-	}
+    res.status(200).send()
+  } else {
+    res.status(401).send({
+      error: "You do not have access to this resource"
+    })
+  }
 }))
 
-// Update specific question
-router.patch('/:id', requireAuthentication, handleErrors( async (req, res, next) => {
-	const question = await getResourceById(Question, req.params.id)
-	const surveyDesign = await getResourceById(SurveyDesign, question.surveyDesignId)
-	
-	if (req.userid == surveyDesign.userId) {
+/*
+ * Update specific question
+ */
+router.patch('/:id', requireAuthentication, handleErrors(async (req, res, next) => {
+  const question = await getResourceById(Question, req.params.id)
+  const surveyDesign = await getResourceById(SurveyDesign, question.surveyDesignId)
 
-		console.log("VISUALIZATION ID: ", req.body.visualizationId)
+  if (req.userid == surveyDesign.userId) {
 
-		let visualizationContentId = question.visualizationContentId
-		if (req.body.visualizationId > 0) {
-			const visualizationImport = await getResourceById(Visualization, req.body.visualizationId)
-			const importInEngine = await visualApi.get(`/${visualizationImport.contentId}`)
-			const importedSvg = importInEngine.data.svg
+    console.log("VISUALIZATION ID: ", req.body.visualizationId)
 
-			if (!visualizationContentId) {
-				 const newVisual = await visualApi.post('/', { svg: importedSvg })
-				 visualizationContentId = newVisual.data.id
-			} else {
-				await visualApi.put(`/${visualizationContentId}`, { svg: importedSvg })
-			}
-		} else if (req.body.visualizationId < 0) {
-			await visualApi.delete(`/${visualizationContentId}`)
-			visualizationContentId = null
-		}
+    let visualizationContentId = question.visualizationContentId
+    if (req.body.visualizationId > 0) {
+      const visualizationImport = await getResourceById(Visualization, req.body.visualizationId)
+      const importInEngine = await visualApi.get(`/${visualizationImport.contentId}`)
+      const importedSvg = importInEngine.data.svg
 
-		await Question.update(req.body, {
-			where: { id: req.params.id },
-			fields: QuestionClientFields.filter(
-			  field => field !== 'surveyDesignId'
-			)
-		})
+      if (!visualizationContentId) {
+        const newVisual = await visualApi.post('/', { svg: importedSvg })
+        visualizationContentId = newVisual.data.id
+      } else {
+        await visualApi.put(`/${visualizationContentId}`, { svg: importedSvg })
+      }
+    } else if (req.body.visualizationId < 0) {
+      await visualApi.delete(`/${visualizationContentId}`)
+      visualizationContentId = null
+    }
 
-		if (visualizationContentId != question.visualizationContentId) {
-			await Question.update({visualizationContentId: visualizationContentId}, {where: { id: req.params.id }})
-		}
+    await Question.update(req.body, {
+      where: { id: req.params.id },
+      fields: QuestionClientFields.filter(
+        field => field !== 'surveyDesignId'
+      )
+    })
 
-		// Update parent survey design's updatedAt timestamp
-		await surveyDesign.changed('updatedAt', true);
-		await surveyDesign.save();
+    if (visualizationContentId != question.visualizationContentId) {
+      await Question.update(
+        { visualizationContentId: visualizationContentId },
+        { where: { id: req.params.id } }
+      )
+    }
 
-		res.status(200).send()
-	} else {
-		res.status(401).send({ 
-			error: "You do not have access to this resource"
-		})
-	}
+    // Update parent survey design's updatedAt timestamp
+    await surveyDesign.changed('updatedAt', true);
+    await surveyDesign.save();
+
+    res.status(200).send()
+  } else {
+    res.status(401).send({
+      error: "You do not have access to this resource"
+    })
+  }
+}))
+
+/*
+ * Move question UP (swap with previous question in same survey)
+ * Trigger this with: POST /questions/:id/moveUp
+ */
+router.post('/:id/moveUp', requireAuthentication, handleErrors(async (req, res, next) => {
+  const question = await getResourceById(Question, req.params.id)
+  const surveyDesign = await getResourceById(SurveyDesign, question.surveyDesignId)
+
+  if (req.userid != surveyDesign.userId) {
+    return res.status(401).send({
+      error: "You do not have access to this resource"
+    })
+  }
+
+  const prevQuestion = await Question.findOne({
+    where: {
+      surveyDesignId: question.surveyDesignId,
+      number: question.number - 1
+    }
+  })
+
+  // Already first or gap in numbering
+  if (!prevQuestion) {
+    return res.status(200).send()
+  }
+
+  await sequelize.transaction(async (t) => {
+    const currentNumber = question.number
+    const prevNumber = prevQuestion.number
+
+    await question.update({ number: prevNumber }, { transaction: t })
+    await prevQuestion.update({ number: currentNumber }, { transaction: t })
+  })
+
+  await surveyDesign.changed('updatedAt', true);
+  await surveyDesign.save();
+
+  res.status(200).send()
+}))
+
+/*
+ * Move question DOWN (swap with next question in same survey)
+ * Trigger this with: POST /questions/:id/moveDown
+ */
+router.post('/:id/moveDown', requireAuthentication, handleErrors(async (req, res, next) => {
+  const question = await getResourceById(Question, req.params.id)
+  const surveyDesign = await getResourceById(SurveyDesign, question.surveyDesignId)
+
+  if (req.userid != surveyDesign.userId) {
+    return res.status(401).send({
+      error: "You do not have access to this resource"
+    })
+  }
+
+  const nextQuestion = await Question.findOne({
+    where: {
+      surveyDesignId: question.surveyDesignId,
+      number: question.number + 1
+    }
+  })
+
+  // Already last or gap in numbering
+  if (!nextQuestion) {
+    return res.status(200).send()
+  }
+
+  await sequelize.transaction(async (t) => {
+    const currentNumber = question.number
+    const nextNumber = nextQuestion.number
+
+    await question.update({ number: nextNumber }, { transaction: t })
+    await nextQuestion.update({ number: currentNumber }, { transaction: t })
+  })
+
+  await surveyDesign.changed('updatedAt', true);
+  await surveyDesign.save();
+
+  res.status(200).send()
 }))
 
 module.exports = router;
